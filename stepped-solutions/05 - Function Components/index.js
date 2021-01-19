324656723a834cfc2b6c6aa428275062b01adc1a
Insidiae <insidiae423@gmail.com>
2021-01-19 17:27:08 +0800

    Finish main part of tic-tac-toe

--- stepped-solutions/05 - Function Components/index.js
+++ stepped-solutions/05 - Function Components/index.js
@@ -18,20 +18,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = [...this.state.squares];
-    if(calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      squares,
-      xIsNext: !this.state.xIsNext,
-    });
+    squares[i] = "X";
+    this.setState({squares});
   }
 
   renderSquare(i) {
@@ -44,14 +37,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
+    const status = 'Next player: X';
 
     return (
       <div>
@@ -98,24 +84,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
-// Calculate the winner of the tic-tac-toe game
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
